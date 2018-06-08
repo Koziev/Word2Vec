a0f30836a38b3934e2f3fe79cfbc1e6a5e3c4b2a
inkoziev <mentalcomputing@gmail.com>
2018-06-08 18:13:10 +0400

    FastText model training launcher

--- PyUtils/train_fasttext.py
+++ PyUtils/train_fasttext.py
@@ -6,17 +6,19 @@ from __future__ import print_function
 import os
 import fasttext
 
-SIZE=64
+SIZE=32
 WINDOW=5
-CBOW=0
+CBOW=1
 MIN_COUNT=1
 
 corpus_path =  os.path.expanduser('~/Corpus/word2vector/ru/SENTx.corpus.w2v.txt')
 
 # Skipgram model
-model = fasttext.skipgram(input_file=corpus_path, dim=SIZE, ws=WINDOW, min_count=1, thread=4, silent=0, output='fasttext_model')
-#print model.words # list of words in dictionary
-
-# CBOW model
-#model = fasttext.cbow(input_file=corpus_path, dim=64, ws=WIN, 'model')
-#print model.words # list of words in dictionary
+filename = 'fasttext.CBOW=' + str(CBOW)+'_WIN=' + str(WINDOW) + '_DIM='+str(SIZE)
+if CBOW == 0:
+    model = fasttext.skipgram(input_file=corpus_path, dim=SIZE, ws=WINDOW, min_count=1, thread=4, silent=0, output=filename)
+    #print model.words # list of words in dictionary
+else:
+    # CBOW model
+    model = fasttext.cbow(input_file=corpus_path, dim=SIZE, ws=WINDOW, min_count=1, thread=4, silent=0, output=filename)
+    #print model.words # list of words in dictionary
